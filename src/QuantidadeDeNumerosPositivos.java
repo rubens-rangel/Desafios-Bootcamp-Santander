5dd328784b39e60b512ed8c2365febebb6a32b3c
rubens-rangel <86197786+rubens-rangel@users.noreply.github.com>
2021-07-27 10:14:38 -0300

    Update and rename DesafioSantander.java to QuantidadeDeNumerosPositivos.java

--- src/QuantidadeDeNumerosPositivos.java
+++ src/QuantidadeDeNumerosPositivos.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class DesafioSantander {
+public class QuantidadeDeNumerosPositivos {
     public static void main(String[] args) throws IOException {
 
         Scanner leitor = new Scanner(System.in);

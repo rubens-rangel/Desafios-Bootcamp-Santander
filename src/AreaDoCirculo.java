d9c791ae6d3ea325bba9f4177b281782e85c8f9d
Rubens Rangel <86197786+rubens-rangel@users.noreply.github.com>
2021-07-27 14:53:13 -0300

    Create AreaDoCirculo.java

---
+++ src/AreaDoCirculo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+
+public class AreaDoCirculo{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		
+
+		 //declare suas variaveis do tipo double
+		 double  raio = scan.nextDouble();
+		 double  area;
+
+                  //continue a solucao
+
+		area = 3.14159 * (Math.pow(raio, 2));
+
+		System.out.printf("A=%.4f\n", area);
+	}
+}

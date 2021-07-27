3b937a3f35ba5e385f4bf98fa954e61d2e2c4356
rubens-rangel <86197786+rubens-rangel@users.noreply.github.com>
2021-07-27 13:07:53 -0300

    Update ContagemDeCedulas.java

--- src/ContagemDeCedulas.java
+++ src/ContagemDeCedulas.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Desafio{
+public class ContagemDeCedulas{
 	
     public static void main(String[] args) throws IOException {
         Scanner leitor = new Scanner(System.in);
